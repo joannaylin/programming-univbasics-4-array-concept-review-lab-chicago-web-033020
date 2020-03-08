7e19aebe0b9648dc562f45485bcea58bc5a05fae
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-08 21:02:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,12 +18,9 @@ def find_element_index(array, value_to_find)
     nil
 end
 
-def find_element_index(array, value_to_find)
-  array.find_index(value_to_find)
-end
-
-
-
+# def find_element_index(array, value_to_find)
+#   array.find_index(value_to_find)
+# end
 
 def find_max_value(array)
   max = 0
