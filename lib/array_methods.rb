e9658537a53fd745f60ffccb914bc2149d9c7f15
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-08 21:02:48 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,6 +9,10 @@
 #   nil
 # end
 
+# def find_element_index(array, value_to_find)
+#   array.find_index(value_to_find)
+# end
+
 def find_element_index(array, value_to_find)
   array.length.times do |count|
     if array[count] == value_to_find
@@ -18,9 +22,8 @@ def find_element_index(array, value_to_find)
     nil
 end
 
-# def find_element_index(array, value_to_find)
-#   array.find_index(value_to_find)
-# end
+
+
 
 def find_max_value(array)
   max = 0
