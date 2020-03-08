6942319965f5db110570dca086c1942a1975202d
Joanna Lin <lin.joanna.y@gmail.com>
2020-03-08 21:15:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -42,13 +42,13 @@ end
 #   max
 # end
 
-def find_max_value(array)
-  x=0
-  array.length.times{
-      |index| x = array[index] if array[index] > x
-  }
-  x
-end
+# def find_max_value(array)
+#   x=0
+#   array.length.times{
+#       |index| x = array[index] if array[index] > x
+#   }
+#   x
+# end
 
 
 def find_min_value(array)
